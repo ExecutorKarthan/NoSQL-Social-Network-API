5b847821f5ff1178614655fe8044b544cefa6a9c
Alex Messina <me@alexmessina.dev>
2024-02-07 20:17:33 -0600

    Updated gitignore and remvoed package-lock

--- utils/seed.js
+++ utils/seed.js
@@ -1,5 +1,5 @@
 const connection = require('../config/connection');
-const { Thought, User } = require('../models');
+const { User } = require('../models');
 const { createUser } = require('./data');
 
 connection.on('error', (err) => err);
